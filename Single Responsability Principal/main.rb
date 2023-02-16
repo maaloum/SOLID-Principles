c8631b60760c50179db2171c05baf66185712bf5
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-02-16 23:18:14 +0300

    now the class shopping cart and invoice are voiloting the SRP

--- Single Responsability Principal/main.rb
+++ Single Responsability Principal/main.rb
@@ -8,9 +8,6 @@ item2 = Item.new("123", 102, 5)
 shopcart = ShoppingCart.new
 stock = Stock.new
 invoice = CreateInvoice.new
-
-# p shopcart.items
-# p shopcart.remove_item(item)
 shopcart.add_item?(item)
 shopcart.add_item?(item2)
 shopcart.cartItems
