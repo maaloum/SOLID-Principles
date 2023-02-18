a36ce309f9aacd46cf189563e511a0c962cf4f7e
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-02-18 16:01:57 +0300

    Liskov Subtitution Principle

--- Liskov Substitution Principle/user.rb
+++ Liskov Substitution Principle/user.rb
@@ -1,9 +1,16 @@
 require 'date'
+require 'ostruct'
 
 class User
-  attr_accessor :settings, :email
+  attr_accessor :email
   def initialize(email:)
     @email = email
+  end
+  def set_settings(level:, status:, signed_in:)
+    @settings = OpenStruct.new(level: level, status: status, signed_in)
+  end
+  def get_settings
+    @settings
   end
 end
 
@@ -11,19 +18,20 @@ class Administator < User
 end
 
 user = User.new(email: "user@example.com")
-user.settings = {level: "low security", status: 'live', signed_in: Date.today}
+user.set_settings(level: "low security", status: 'live', signed_in: Date.today)
 admin = Administator.new(email: "admin@example.com")
-admin.settings = ["Editor", "VIP", Date.today]
+admin.set_settings(level: "Editor", status: "VIP", signed_in: Date.today)
 
 p user.settings
 p admin.settings
 
 @user_database = [user, admin]
 def signed_in_today?
-  @user_database.each do
-    |user| if user.settings[:signed_in] == Date.today
-              puts "#{user.email} signed today"
+  @user_database.each do |user|
+    if user.get_settings.signed_in == Date.today
+      puts "#{user.email} signed today"
     end
   end
 end
-signed_in_today?
+
+# p signed_in_today?
